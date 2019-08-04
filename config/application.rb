1bef23940c49591d31b816b26540926b7cfd00c2
Pelino <e7.ifms@gmail.com>
2019-08-04 11:46:14 -0400

    put active record

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require "active_record/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
