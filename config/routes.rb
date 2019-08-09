5e6e7e5c2e62a1d153086a4156a1b9b690ad261d
Pelino <e7.ifms@gmail.com>
2019-08-08 23:20:37 -0400

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'heroes' => 'heroes#index', as: :heroes
+  root 'heroes#index', as: :heroes
+
   get 'heroes/:id' => 'heroes#show', as: :heroe
   get 'heroes/:id/comics' => 'heroes#comics', as: :heroe_comics
 
-  
+
 end
